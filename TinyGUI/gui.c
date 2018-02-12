9e55d8885ed1b03b3dd0dee5860a29f301e1b638
CanftIn <wwc7033@gmail.com>
2018-02-12 20:27:33 +0800

    update

--- TinyGUI/gui.c
+++ TinyGUI/gui.c
@@ -10,4 +10,43 @@
 #define GUI_NULL (void*)0
 #define PI 3.141592654f
 
+#define PASTE(a,b) a##b
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) < (b) ? (b) : (a))
+#define CLAMP(i,v,x) (MAX(MIN(v,x), i))
+#define LEN(a) (sizeof(a)/sizeof(a)[0])
+#define ABS(a) (((a) < 0) ? -(a) : (a))
+#define UNUSED(a) ((void)(a))
+#define DEG2RAD(a) ((a) * (PI / 180.0f))
+#define RAD2DEG(a) ((a) * (180.0f / PI))
+#define BETWEEN(x, a, b) ((a) <= (x) && (x) <= (b))
+#define INBOX(x, y, x0, y0, x1, y1) (BETWEEN(x, x0, x1) && BETWEEN(y, y0, y1))
+
+#define ASSERT_LINE(p, l, f) \
+    typedef char PASTE(assertion_failed_##f##_,l)[2*!!(p)-1];
+#define ASSERT(predicate) ASSERT_LINE(predicate,__LINE__,__FILE__)
+
+#define vec2_load(v,a,b) (v).x = (a), (v).y = (b)
+#define vec2_cpy(to,from) (to).x = (from).x, (to).y = (from).y
+#define vec2_len(v) ((float)fsqrt((v).x*(v).x+(v).y*(v).y))
+#define vec2_sub(r,a,b) do {(r).x=(a).x-(b).x; (r).y=(a).y-(b).y;} while(0)
+#define vec2_add(r,a,b) do {(r).x=(a).x+(b).x; (r).y=(a).y+(b).y;} while(0)
+#define vec2_muls(r, v, s) do {(r).x=(v).x*(s); (r).y=(v).y*(s);} while(0)
+#define vec2_norm(r, v) do {float _ = vec2_len(v); if (_) vec2_muls(r, v, 1.0f/_);} while(0)
+
+static const gui_texture null_tex = (void*)0;
+static const struct gui_rect null_rect = {-9999, -9999, 9999 * 2, 9999 * 2};
+
+static gui_float
+fsqrt(float x)
+{
+    float xhalf = 0.5f*x;
+    union {float f; int i;} val;
+    ASSERT(sizeof(int) == sizeof(float));
+
+    val.f = x;
+    val.i = 0x5f375a86 - (val.i>>1);
+    val.f = val.f*(1.5f-xhalf*val.f*val.f);
+    return 1.0f/val.f;
+}
 
